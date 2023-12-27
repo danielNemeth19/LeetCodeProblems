5ebcc11f1ce838b70b2a032e20e05817797a12f9
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2023-12-27 23:49:25 +0100

    (feat) Faster python solution for the two sums problem

--- python/two_sums.py
+++ python/two_sums.py
@@ -20,9 +20,20 @@ class Solution:
         d_nums = dict(enumerate(nums))
         for i, x in enumerate(nums):
             y = target - x
-            for k, v in d_nums.items():
-                if v == y and i != k:
-                    return [i, k]
+            if y in nums:
+                for k, v in d_nums.items():
+                    if v == y and i != k:
+                        return [i, k]
+        return []
+
+    def two_sum3(self, nums: List[int], target: int) -> List[int]:
+        d_nums = {num: i for i, num in enumerate(nums)}
+        for i, x in enumerate(nums):
+            y = target - x
+            if y in d_nums:
+                for j, v in enumerate(nums):
+                    if v == y and i != j:
+                        return [i, j]
         return []
 
 
@@ -37,7 +48,7 @@ def parse_args():
 def main():
     nums, target = parse_args()
     s = Solution()
-    result = s.two_sum2(nums=nums, target=target)
+    result = s.two_sum3(nums=nums, target=target)
     print(result)
 
 
