e464d4c07298a3ec6ea2d5b0aa57d57841ff45bb
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-01-03 00:03:25 +0100

    (feat) Faster python solution for the max_length_between_chars

--- python/max_length_between_chars.py
+++ python/max_length_between_chars.py
@@ -27,6 +27,17 @@ class Solution:
             self.max_length = current_max_lenght if current_max_lenght > self.max_length else self.max_length
         return self.max_length
 
+    def max_length_between_equal_chars2(self, s: str) -> int:
+        max_length = -1
+        for c in set(s):
+            length = s.rfind(c) - s.find(c) - 1
+            max_length = length if length > max_length else max_length
+        return max_length
+
+    # one-liner solution from comments
+    def max_length_between_equal_chars_not3(self, s: str) -> int:
+        return max(s.rfind(c) - s.find(c) - 1 for c in set(s))
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -38,7 +49,7 @@ def parse_args():
 def main():
     input_s = parse_args()
     s = Solution()
-    result = s.max_length_between_equal_chars(input_s)
+    result = s.max_length_between_equal_chars2(input_s)
     print(f"Verdict: {result}")
 
 
