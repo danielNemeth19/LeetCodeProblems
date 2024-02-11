2c5ac0e78758e74fbb299d3d73d41366453d5337
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-02-11 18:16:56 +0100

    (challenge) Merge two sorted list problem

---
+++ python/merge_two_sorted_lists.py
@@ -0,0 +1,13 @@
+"""
+Definiton for singly-linked list
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+"""
+from typing import Optional
+
+
+class Solution:
+    def merge_two_lists(self, list1: Optional[ListNode], list2: Optional[ListNode]):
+        return
