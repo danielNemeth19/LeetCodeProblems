c3bd4d3ad42826581b3d451396465da5d3501d7b
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-12-23 18:15:06 +0100

    (feat) Python solution for merge two sorted list

---
+++ problems/merge_two_sorted_list.md
@@ -0,0 +1,49 @@
+## Merge Two Sorted Lists
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+### Example 1:
+* Input: list1 = [1,2,4], list2 = [1,3,4]
+* Output: [1,1,2,3,4,4]
+
+### Example 2:
+* Input: list1 = [], list2 = []
+* Output: []
+
+### Example 3:
+* Input: list1 = [], list2 = [0]
+* Output: [0]
+
+### Constraints:
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+
+
+### Additional notes from python solution
+* Manually creating a linked list for testing input
+```
+def manual_example(data: list):
+    n = ListNode(data[0])
+    n2 = ListNode(data[1])
+    n3 = ListNode(data[2])
+    n.next = n2
+    n2.next = n3
+    return n
+```
+
+* Primitive way to create a ListNode
+```
+def create_list_node_old(data: list):
+    nodes = []
+    for i in data:
+        node = ListNode(i)
+        nodes.append(node)
+    for i, node in enumerate(nodes):
+        if i < len(nodes) - 1:
+            node.next = nodes[i+1]
+    return nodes[0]
+```
--- python/merge_two_sorted_lists.py
+++ python/merge_two_sorted_lists.py
@@ -1,13 +1,83 @@
-"""
-Definiton for singly-linked list
+import argparse
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-"""
-from typing import Optional
+
+    # not required and used in solution
+    # but the linked list can be made iterable like this
+    def __iter__(self):
+        self.current = self
+        return self
+
+    def __next__(self):
+        if self.current is None:
+            raise StopIteration
+        previous = self.current
+        self.current = self.current.next
+        return previous
+
+    def __str__(self):
+        # ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: None}}}
+        return f"ListNode{{val: {self.val}, next:{self.next}}}"
 
 
 class Solution:
-    def merge_two_lists(self, list1: Optional[ListNode], list2: Optional[ListNode]):
-        return
+    def merge_two_lists_solution(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        h1 = self.get_head_values_without_iter(list1)
+        h2 = self.get_head_values_without_iter(list2)
+        return self.create_list_node(sorted(h1 + h2))
+
+    @staticmethod
+    def create_list_node(data: list):
+        if not data:
+            return []
+        node, current_node = None, None
+        for i, val in enumerate(data):
+            if node is None:
+                node = ListNode(val)
+                current_node = node
+            if i < len(data) - 1:
+                next_node = ListNode(data[i+1])
+                current_node.next = next_node
+                current_node = next_node
+        return node
+
+    def get_head_values_without_iter(self, l_list: Optional[ListNode]) -> list:
+        if not l_list:
+            return []
+        heads = []
+        current = l_list
+        while True:
+            v = current.val
+            heads.append(v)
+            if current.next is not None:
+                current = current.next
+            else:
+                break
+        return heads
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--l1', nargs="+", type=int, default=[])
+    parser.add_argument('--l2', nargs="+", type=int, default=[])
+    args = parser.parse_args()
+    return args.l1, args.l2
+
+
+def main():
+    s = Solution()
+    l1, l2 = parse_args()
+    node1 = s.create_list_node(l1)
+    node2 = s.create_list_node(l2)
+
+    sol = s.merge_two_lists_solution(node1, node2)
+    print(sol)
+
+
+if __name__ == "__main__":
+    main()
